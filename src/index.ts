aa17b36a49faf139a038aac191da321db922f45f
Richie Casto <rcasto92@gmail.com>
2019-07-13 11:48:33 -0700

    test add to quadtree, and adding implementation

--- src/index.ts
+++ src/index.ts
@@ -34,10 +34,84 @@ export function addToQuadTree(quadTree: QuadTree, object: CollisionObject): bool
     // Below is the self check (this node)
     // This is the base case that is ran for all nodes
 
-    if (quadTree.data.length + 1 <= quadTree.capacity) {
-        quadTree.data.push(object);
+    // Let's first check if the objects bounding box intersects
+    // with this quadrants bounding box
+    const objectBoundingBox: BoundingBox = object.getBoundingBox();
+
+    if (!doBoundingBoxesIntersect(quadTree.bounds, objectBoundingBox)) {
+        return false;
     }
 
+    // Let's see if this quadrant has any capacity
+    // If it does, we can go ahead and store the current object
+    if (quadTree.data.length + 1 <= quadTree.capacity) {
+        quadTree.data.push(object);
+        return true;
+    }
+
+    // The current node fits the current object, but
+    // There isn't any capacity
+    // We need to split this quadrant up
+
+    // Let's first build the child quadrants
+    const quadWidth: number = quadTree.bounds.width / 2;
+    const quadHeight: number = quadTree.bounds.height / 2;
+
+    const nwBoundingBox: BoundingBox = {
+        x: 0,
+        y: 0,
+        width: quadWidth,
+        height: quadHeight,
+    };
+    const neBoundingBox: BoundingBox = {
+        x: quadWidth,
+        y: 0,
+        width: quadWidth,
+        height: quadHeight,
+    };
+    const swBoundingBox: BoundingBox = {
+        x: 0,
+        y: quadHeight,
+        width: quadWidth,
+        height: quadHeight,
+    };
+    const seBoundingBox: BoundingBox = {
+        x: quadWidth,
+        y: quadHeight,
+        width: quadWidth,
+        height: quadHeight,
+    };
+
+    // Let's create the child QuadTree's from the build quadrant bounds
+    const quadrants: QuadTree[] = [
+        createQuadTree(nwBoundingBox, quadTree.capacity),
+        createQuadTree(neBoundingBox, quadTree.capacity),
+        createQuadTree(swBoundingBox, quadTree.capacity),
+        createQuadTree(seBoundingBox, quadTree.capacity),
+    ];
+    const quadObjects: CollisionObject[] = [...quadTree.data, object];
+
+    // adjust current quadtree settings
+    // May need to adjust these in-place instead of creating new references
+    quadTree.quadrants = quadrants;
+    quadTree.data = [];
+
+    // add objects from this quad node back to it's own subtree
+    // children will be attempted to be added to first
+    return quadObjects
+        .every(quadObject => addToQuadTree(quadTree, quadObject));
+}
+
+function doBoundingBoxesIntersect(box1: BoundingBox, box2: BoundingBox): boolean {
+    // If one rectangle is to the side of other (outside)
+    if (box1.x > box2.x + box2.width || box2.x > box1.x + box1.width) {
+        return false;
+    }
+
+    // If one rectangle is above the other (outside)
+    if (box1.y > box2.y + box2.height || box2.y > box1.y + box1.height) {
+        return false;
+    }
 
     return true;
 }
--- src/test.ts
+++ src/test.ts
@@ -2,17 +2,22 @@
 import { BoundingBox, CollisionObject } from "./schemas";
 import { createQuadTree, addToQuadTree } from "./index";
 
+interface Box extends CollisionObject {
+    id: string;
+}
+
 const bounds: BoundingBox = {
     x: 0,
     y: 0,
     width: 400,
     height: 400,
 };
-const quadTree = createQuadTree(bounds);
+const quadTree = createQuadTree(bounds, 3);
 
 console.log(quadTree);
 
-const box1: CollisionObject = {
+const nwBox: Box = {
+    id: "northwest",
     getBoundingBox: () => {
         return {
             x: 0,
@@ -22,7 +27,46 @@ const box1: CollisionObject = {
         };
     }
 };
+const neBox: Box = {
+    id: "northeast",
+    getBoundingBox: () => {
+        return {
+            x: bounds.width / 2 + 1,
+            y: 0,
+            width: 20,
+            height: 20,
+        };
+    }
+};
+const swBox: Box = {
+    id: "southwest",
+    getBoundingBox: () => {
+        return {
+            x: 0,
+            y: bounds.height / 2 + 1,
+            width: 20,
+            height: 20,
+        };
+    }
+};
+const seBox: Box = {
+    id: "southeast",
+    getBoundingBox: () => {
+        return {
+            x: bounds.width / 2 + 1,
+            y: bounds.height / 2 + 1,
+            width: 20,
+            height: 20,
+        };
+    }
+};
 
-addToQuadTree(quadTree, box1);
+addToQuadTree(quadTree, nwBox);
+addToQuadTree(quadTree, neBox);
+addToQuadTree(quadTree, swBox);
+
+// Adding 1 more instance of the box
+// exceeds the nodes capacity, triggering a split
+addToQuadTree(quadTree, seBox);
 
 console.log(quadTree);
