c4f9e04413c35b6fdc509a4086f71b28489a7bdf
Richie Casto <rcasto92@gmail.com>
2019-07-14 18:50:19 -0700

    add point interface

--- src/schemas.ts
+++ src/schemas.ts
@@ -1,6 +1,9 @@
-export interface BoundingBox {
+export interface Point {
     x: number;
     y: number;
+}
+
+export interface BoundingBox extends Point {
     width: number;
     height: number;
 }
