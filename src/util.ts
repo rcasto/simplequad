b4b0b0bfc32ea7a700bf818bbb7b989e88889acf
Richie Casto <rcasto92@gmail.com>
2019-07-18 20:35:01 -0700

    refactor and reuse logic

--- src/util.ts
+++ src/util.ts
@@ -1,24 +1,21 @@
 import { BoundingBox, Point } from './schema';
 
 export function containsPoint(bounds: BoundingBox, point: Point) {
-    return point.x >= bounds.x && 
-           point.x <= bounds.x + bounds.width &&
-           point.y >= bounds.y &&
-           point.y <= bounds.y + bounds.height;
+    return doBoundingBoxesIntersect(bounds, {
+        x: point.x,
+        y: point.y,
+        width: 0,
+        height: 0,
+    });
 }
 
 export function doBoundingBoxesIntersect(box1: BoundingBox, box2: BoundingBox): boolean {
-    // If one rectangle is to the side of other (outside)
-    if (box1.x > box2.x + box2.width || box2.x > box1.x + box1.width) {
-        return false;
-    }
-
-    // If one rectangle is above the other (outside)
-    if (box1.y > box2.y + box2.height || box2.y > box1.y + box1.height) {
-        return false;
-    }
-
-    return true;
+    return (
+        box1.x <= box2.x + box2.width &&
+        box1.x + box1.width >= box2.x &&
+        box1.y <= box2.y + box2.height &&
+        box1.y + box1.height >= box2.y
+    );
 }
 
 export function divideBoundingBox(bounds: BoundingBox): BoundingBox[] {
