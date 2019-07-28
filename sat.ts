5c0118d313402f10c2ea653a0075dfe757dc6518
Richie Casto <rcasto92@gmail.com>
2019-07-28 09:46:32 -0700

    stash

---
+++ sat.ts
@@ -0,0 +1,12 @@
+import { BoundingBox, Point } from './src/schema';
+
+interface RotatedBoundingBox extends BoundingBox {
+    // This property goes from 0 -> 2 * pi
+    rot: number;
+}
+
+function get
+
+function normal() {
+
+}
